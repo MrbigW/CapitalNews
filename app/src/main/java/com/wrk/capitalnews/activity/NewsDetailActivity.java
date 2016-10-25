0086a723ac2156f158b41f90fc6a9593f908fcc2
[Window Title] <1024057635@qq.com>
2016-10-25 21:38:11 +0800

    使用ShareSDK实现分享功能

--- app/src/main/java/com/wrk/capitalnews/activity/NewsDetailActivity.java
+++ app/src/main/java/com/wrk/capitalnews/activity/NewsDetailActivity.java
@@ -26,6 +26,8 @@ import com.wrk.capitalnews.view.NewsDetialPopUpWindow;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 
 public class NewsDetailActivity extends AppCompatActivity {
 
@@ -47,6 +49,7 @@ public class NewsDetailActivity extends AppCompatActivity {
     private NewsDetialPopUpWindow mMPopupWindow;
     private int screenWidth;
     private int screenHeight;
+    private Uri mUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,8 +64,8 @@ public class NewsDetailActivity extends AppCompatActivity {
     }
 
     private void getData() {
-        Uri uri = getIntent().getData();
-        if (uri != null) {
+        mUri = getIntent().getData();
+        if (mUri != null) {
             mWebSettings = webview.getSettings();
             mWebSettings.setJavaScriptEnabled(true);
             mWebSettings.setBuiltInZoomControls(true);
@@ -74,7 +77,7 @@ public class NewsDetailActivity extends AppCompatActivity {
                     pbLoad.setVisibility(View.GONE);
                 }
             });
-            webview.loadUrl(String.valueOf(uri));
+            webview.loadUrl(String.valueOf(mUri));
         }
     }
 
@@ -88,7 +91,7 @@ public class NewsDetailActivity extends AppCompatActivity {
                 showChangeTextDialog();
                 break;
             case R.id.icon_share:
-
+                showShare();
                 break;
             case R.id.fab_newsdetail_more:
 
@@ -105,6 +108,7 @@ public class NewsDetailActivity extends AppCompatActivity {
                         PropertyValuesHolder p2 = PropertyValuesHolder.ofFloat("translationY", -y, 0);
                         PropertyValuesHolder p3 = PropertyValuesHolder.ofFloat("rotation", 720 - 45, 0);
                         ObjectAnimator.ofPropertyValuesHolder(fabNewsdetailMore, p1, p2, p3).setDuration(700).start();
+
                     }
                 });
 
@@ -122,7 +126,6 @@ public class NewsDetailActivity extends AppCompatActivity {
 
                     @Override
                     public void onAnimationEnd(Animator animation) {
-
                         mMPopupWindow.showAtLocation(webview, Gravity.BOTTOM, 0, 0);
                     }
 
@@ -142,7 +145,6 @@ public class NewsDetailActivity extends AppCompatActivity {
                 break;
         }
     }
-
 
 
     public void getScreenWidthAndHeight() {
@@ -201,7 +203,47 @@ public class NewsDetailActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
 
+        if (mMPopupWindow != null && mMPopupWindow.isShowing()) {
+            mMPopupWindow.dismiss();
+            mMPopupWindow = null;
+        }
+
+    }
+
+    private void showShare() {
+        ShareSDK.initSDK(this);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+        // 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle("CapitalNews");
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("哎呦不错哦");
+        //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
+        oks.setImageUrl("http://f.hiphotos.baidu.com/image/h%3D360/sign=131b9d890d2442a7b10efba3e143ad95/4d086e061d950a7b8f42e9ce08d162d9f2d3c941.jpg");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl(String.valueOf(mUri));
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("哎呦，不错哦");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite("CapitalNews");
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl(String.valueOf(mUri));
+
+        // 启动分享GUI
+        oks.show(this);
+    }
 }
 
 
---
+++ app/src/main/java/com/wrk/capitalnews/apshare/ShareEntryActivity.java
@@ -0,0 +1,7 @@
+package com.wrk.capitalnews.apshare;
+
+import cn.sharesdk.alipay.utils.AlipayHandlerActivity;
+
+public class ShareEntryActivity extends AlipayHandlerActivity{
+
+}
